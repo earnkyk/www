eef125ac810a139cfc814af942d45b7ee185df31
earnkyk <73695377+earnkyk@users.noreply.github.com>
2020-10-30 17:46:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,17 +3,21 @@
         <title>Hello world</title>
     </head>
     <body>
-        <h1>My First Heading</h1>
+        <h1>กัลยกร วัชรรัตนากุล เอิน</h1>
         <p>My First Paragraph</p>
         <img src="cat.jpg">
+        <img src="cat2.jpg">
         <br>
         <a href="http://it.kmitl.ac.th">This is a Link</a>
 
-        <h2>The unordered list</h2>
+        <h2>เรื่องตลก</h2>
         <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
+            <li> พ่อครับๆ"ลูก ชายตะโกนจากในสวน
+                "รถถูกขโมย"
+                "เห็นไหมว่าใครเอาไป"
+                " เห็นครับ"
+                "จำหน้าได้ไหม"
+                "จำไม่ได้ แต่ผมจดทะเบียนรถเราไว้แล้ว"</li>
         </ul>
     
     </body>
